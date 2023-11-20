c1fc1e72a32d6dfcc3da886a13057879d04ab946
Cyril Olanolan <57930177+CyrilOlanolan@users.noreply.github.com>
2023-11-20 14:33:41 +0800

    fix: button failed test

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -4,6 +4,7 @@ const Button = ({ onClick, children, ...props }) => {
   return (
     <button
       className={`rounded-md p-2 m-2 disabled:bg-gray-300 bg-blue-500 text-white`}
+      onClick={onClick}
       {...props}
     >
       {children}
